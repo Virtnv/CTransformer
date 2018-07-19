b641acb97cb477f20a4f86306103ecd9c3e5132c
Dmitriy Nikolaev <dmnikolaev@outlook.com>
2018-07-19 12:22:06 +0500

    Commit 1

--- CTransformer/Program.cs
+++ CTransformer/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,51 @@ namespace CTransformer
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                System.Console.WriteLine("Please enter a file name to transform");
+                return 1;
+            }
+            string fileName = args[0];
+            string fileExtension = fileName.Substring(fileName.Length - 4);
+            if(fileExtension != ".bin")
+            {
+                Console.WriteLine("Uncorrect file format!");
+                return 1;
+            }
+
+            using (FileStream fStream = File.OpenRead(Directory.GetCurrentDirectory() + "fileName"))
+            {
+                if (fStream.Length < 640)
+                {
+                    Console.WriteLine("Too small file!");
+                    return 1;
+                }
+            }
+            //if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            //{
+            //    FileInfo fInfo = new FileInfo(openFileDialog1.FileName);
+            //    if (fInfo.Length < 640)
+            //    {
+            //        InfoBlock.Text += "\nFile size < 640 bytes!";
+            //    }
+            //    else
+            //    {
+            //        using (fStream = File.OpenRead(openFileDialog1.FileName))
+            //        {
+            //            //fStream.Read(b, 0, b.Length);
+                        
+                        //ph = (PanelHeader)Serializer.RawDeserialize(b, 0, typeof(PanelHeader));
+
+                 
+
+
+                //fileName.Substring(".bin");
+                Console.WriteLine("Everythiing OK!");
+            Console.ReadLine();
+            return 0;
         }
     }
 }
---
+++ CTransformer/Serializer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transformer
+{
+    static class Serializer
+    {
+        public static object RawDeserialize(byte[] rawData, int position, Type anyType)
+        {
+            int rawsize = Marshal.SizeOf(anyType);
+            if (rawsize > rawData.Length)
+                return null;
+            IntPtr buffer = Marshal.AllocHGlobal(rawsize);
+            Marshal.Copy(rawData, position, buffer, rawsize);
+            object retobj = Marshal.PtrToStructure(buffer, anyType);
+            Marshal.FreeHGlobal(buffer);
+            return retobj;
+        }
+
+        public static byte[] RawSerializeShort(object anything)
+        {
+            int rawsize = Marshal.SizeOf(anything);
+            byte[] rawdata = new byte[rawsize];
+            GCHandle handle = GCHandle.Alloc(rawdata, GCHandleType.Pinned);
+            Marshal.StructureToPtr(anything, handle.AddrOfPinnedObject(), false);
+            handle.Free();
+            return rawdata;
+        }
+
+        public static byte[] RawSerialize(object anything)
+        {
+            int rawSize = Marshal.SizeOf(anything);
+            IntPtr buffer = Marshal.AllocHGlobal(rawSize);
+            Marshal.StructureToPtr(anything, buffer, false);
+            byte[] rawDatas = new byte[rawSize];
+            Marshal.Copy(buffer, rawDatas, 0, rawSize);
+            Marshal.FreeHGlobal(buffer);
+            return rawDatas;
+        }
+    }
+}
