b1caafb24ab823ea13b80fce7db88666fd4d64c0
Dmitriy Nikolaev <dmnikolaev@outlook.com>
2018-07-20 12:55:34 +0500

    Parser begin

---
+++ CTransformer/CommandLineParser.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+
+namespace CTransformer
+{
+    class CommandLineParser // парсит аргументы командной строки
+    {
+        static public FileStream InputFileNameParse(string inputFileName)
+        {
+            string fileExt = Path.GetExtension(inputFileName);
+            if (fileExt.Length == 0)
+            {
+                fileExt = "bin";
+            }
+            else if (fileExt != "bin")
+            {
+                Console.WriteLine("Incorrect file extension!");
+                return null;
+
+            }
+            return null;
+        }
+
+        public static string[] Splitter(string stringToSplit)
+        {
+            stringToSplit = stringToSplit.Trim();
+            string []split = stringToSplit.Split(new Char[] { ' ', '\t' });
+            return split;
+        }
+            //Console.WriteLine($"{File.Exists(inputFileName)}");
+            //// Save all needed types
+            //string fullPath = Path.GetFullPath(inputFileName);
+            //string directoryName = Path.GetDirectoryName(inputFileName);
+            //string fileName = Path.GetFileName(inputFileName);
+            //string fileNameWithoutExt = Path.GetFileNameWithoutExtension(inputFileName);
+
+            //string[] sa = { fullPath, directoryName, fileName, fileExt, fileNameWithoutExt };
+            //foreach (var item in sa)
+            //{
+            //    Console.WriteLine($"{inputFileName} - {item}");
+            //}
+
+            //if (fileName.Length != 0 && fileExt == ".bin")
+            //{
+            //    try
+            //    {
+            //        FileStream fs = new FileStream(directoryName + @"\" + fileName, FileMode.Open);
+            //    }
+            //    catch (Exception e)
+            //    {
+
+            //        //throw;
+            //    }
+            //}
+
+            //string path = inputFileName;
+            //Console.WriteLine(Path.GetExtension(path));
+        
+    }
+}
--- CTransformer/Program.cs
+++ CTransformer/Program.cs
@@ -11,43 +11,79 @@ namespace CTransformer
     {
         static int Main(string[] args)
         {
-            if (args.Length == 0)
-            {
-                System.Console.WriteLine("Please enter a file name to transform");
-                return 1;
-            }
-            string fileName = args[0];
-            string fileExtension = fileName.Substring(fileName.Length - 4);
-            if(fileExtension != ".bin")
-            {
-                Console.WriteLine("Incorrect file format!");
-                return 1;
-            }
-            byte[] b = new byte[640];
-            try
-            {
-                using (FileStream fStream = File.OpenRead(Directory.GetCurrentDirectory() + $"\\{fileName}"))
-                {
-                    if (fStream.Length < 640)
-                    {
-                        Console.WriteLine("Too small file!");
-                        return 1;
-                    }
-                    fStream.Read(b, 0, b.Length);
-                    //ph = (PanelHeader)Serializer.RawDeserialize(b, 0, typeof(PanelHeader));
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-                Console.Read();
-                return 1;
-            }
-            
+            string line;
+            char quitChar = 'q';
+            Console.WriteLine("***** CTransformer .bin file converter *****");
+            Console.WriteLine($"    ... Press '{quitChar}' for exit ...");
 
-            Console.WriteLine("Everythiing OK!");
+            while ((line = Console.ReadLine()) != quitChar.ToString())
+            {
+                if (args == null)
+                {
+                    args = CommandLineParser.Splitter(line);
+                    //TO DO: сплиттер должен убирать из строки повторяющиеся пробелы и табуляцию
+                }
+                if (args.Length > 0)
+                {
+                    try
+                    {
+                        CommandLineParser.InputFileNameParse(args[0]); //TO DO: заебенить проверку на ввод хелпа!
+
+                    }
+                    catch (Exception)
+                    {
+
+                        //throw;
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Enter the parameters!");
+                }
+                //Console.WriteLine(args[0]);
+                args = null;
+            }
+
+            //if (args.Length == 0)
+            //{
+            //    System.Console.WriteLine("Please enter a file name to transform");
+            //    return 1;
+            //}
+            //string fileName = args[0];
+            ////string fileExtension = fileName.Substring(fileName.Length - 4);
+            //string fileExtension = ".bin";
+            //if (fileExtension != ".bin")
+            //{
+            //    Console.WriteLine("Incorrect file format!");
+            //    return 1;
+            //}
+            //byte[] b = new byte[640];
+            //try
+            //{
+            //    using (FileStream fStream = File.OpenRead(Directory.GetCurrentDirectory() + $"\\{fileName}"))
+            //    {
+            //        if (fStream.Length < 640)
+            //        {
+            //            Console.WriteLine("Too small file!");
+            //            return 1;
+            //        }
+            //        fStream.Read(b, 0, b.Length);
+            //        //ph = (PanelHeader)Serializer.RawDeserialize(b, 0, typeof(PanelHeader));
+            //    }
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine(e.Message);
+            //    Console.Read();
+            //    return 1;
+            //}
+
+
+            Console.WriteLine("Everything OK!");
             Console.ReadLine();
             return 0;
         }
+
     }
 }
